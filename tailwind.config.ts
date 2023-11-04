292214338531a3e69787a67d71a47c5c5b7f155c
rajput-hemant <rajput.hemant2001@gmail.com>
2023-11-04 22:55:40 +0530

    init
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        "2xl": "1280px",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
+
+export default config;
